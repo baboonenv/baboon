48a8231822ef1b6f207d612f53a51c09da5278ca
Behram ÇELEN <behramcelen@gmail.com>
2016-12-01 03:56:27 +0300

    setup theme server related entity and orm files

---
+++ src/Baboon/PanelBundle/Entity/ThemeServer.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Baboon\PanelBundle\Entity;
+
+/**
+ * Class ThemeServer
+ * @package Baboon\PanelBundle\Entity
+ */
+class ThemeServer
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $url;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     *
+     * @return $this
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+}
