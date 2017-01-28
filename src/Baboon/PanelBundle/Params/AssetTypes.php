2f75476d5772502e945d47f115dff16702458a34
Behram ÇELEN <behramcelen@gmail.com>
2017-01-28 17:01:10 +0300

    create asset types param bag

---
+++ src/Baboon/PanelBundle/Params/AssetTypes.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Baboon\PanelBundle\Params;
+
+class AssetTypes
+{
+    const TEXT = 'text';
+    const STRING = 'string';
+    const MARKDOWN = 'markdown';
+    const RICHTEXT = 'richtext';
+    const IMAGE = 'image';
+    const FILE = 'file';
+    const DATETIME = 'datetime';
+    const CHOICES = 'choices';
+    const EMAIL = 'email';
+    const COLOR = 'color';
+}
