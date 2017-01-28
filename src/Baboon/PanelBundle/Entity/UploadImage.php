f3827f461edd9821ea79cc1b12db38947619b354
Behram ÇELEN <behramcelen@gmail.com>
2017-01-28 17:03:52 +0300

    create image field getter setters

--- src/Baboon/PanelBundle/Entity/UploadImage.php
+++ src/Baboon/PanelBundle/Entity/UploadImage.php
@@ -9,4 +9,24 @@ namespace Baboon\PanelBundle\Entity;
 class UploadImage
 {
     protected $image;
+
+    /**
+     * @return string|null
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param string|null $image
+     *
+     * @return $this
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
 }
