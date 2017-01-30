4a0f42bc9835d73f32268c26a0b13f26c2fbd050
Behram ÇELEN <behramcelen@gmail.com>
2017-01-30 11:30:52 +0300

    ftp configuration system full setup :tada:

--- src/Baboon/PanelBundle/Controller/DeployController.php
+++ src/Baboon/PanelBundle/Controller/DeployController.php
@@ -2,7 +2,11 @@
 
 namespace Baboon\PanelBundle\Controller;
 
+use Baboon\PanelBundle\Entity\FTPConfiguration;
+use Baboon\PanelBundle\Form\FTPConfigurationType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DeployController extends Controller
@@ -13,5 +17,125 @@ class DeployController extends Controller
         $deployService->syncSiteTheme();
 
         return new Response('successful');
+    }
+
+    public function FTPisConfiguredAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $ftpConfiguration = $em->getRepository(FTPConfiguration::class)->findOneBy([]);
+        if($ftpConfiguration){
+            return new JsonResponse([
+                'isConfigured' => true
+            ]);
+        }
+
+        return new JsonResponse([
+            'isConfigured' => false
+        ]);
+    }
+
+    public function configureFTPAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $FTPConfiguration = $em->getRepository(FTPConfiguration::class)->findOneBy([]);
+        if(!$FTPConfiguration){
+            $FTPConfiguration = new FTPConfiguration();
+        }
+        $form = $this->createForm(FTPConfigurationType::class, $FTPConfiguration, [
+            'action' => $this->generateUrl('bb_panel_deploy_configure_ftp')
+        ]);
+        $form->handleRequest($request);
+
+        if($request->getMethod() == 'POST' && $form->isValid()){
+            $em->persist($FTPConfiguration);
+            $em->flush();
+            $this->addFlash('success', 'successful.update');
+        }
+
+        return $this->render('@BaboonPanel/Deploy/_ftp_configuration.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    public function FTPConnectionTestAction(Request $request)
+    {
+        $FTPConfiguration = new FTPConfiguration();
+        $form = $this->createForm(FTPConfigurationType::class, $FTPConfiguration, [
+            'action' => $this->generateUrl('bb_panel_deploy_configure_ftp')
+        ]);
+        $form->handleRequest($request);
+        $errors = [];
+        $successes = [];
+
+        //test connection is valid
+        try{
+            $FTPConnect = ftp_connect($FTPConfiguration->getHostname());
+            $successes[] = 'host.works.correctly';
+        }catch (\Exception $exception){
+            $errors[] = 'host.connection.refused';
+        }
+
+        //test login is valid
+        try{
+            if(empty($errors)){
+                ftp_login($FTPConnect, $FTPConfiguration->getUsername(), $FTPConfiguration->getPassword());
+                $successes[] = 'ftp.login.successful';
+            }
+        }catch (\Exception $exception){
+            $errors[] = 'ftp.login.failed';
+        }
+
+        //test dir exists
+        try{
+            if(empty($errors)){
+                ftp_chdir($FTPConnect, $FTPConfiguration->getPath());
+                $successes[] = 'ftp.dir.exists';
+            }
+        }catch (\Exception $exception){
+            $errors[] = 'i.could.not.find.path';
+        }
+
+        //test put granted
+        try{
+            if(empty($errors)){
+                $fileName = 'ftp_test_file.html';
+                $FTPPath = $FTPConfiguration->getPath();
+                $FTPPath = (substr($FTPPath,-1)!='/') ? $FTPPath.='/' : $FTPPath;
+                $putPath = $FTPPath.$fileName;
+                $testFile = $this->get('kernel')->getRootDir().'/../web/'.$fileName;
+                $fopen = fopen($testFile, 'r');
+                ftp_fput($FTPConnect, $putPath, $fopen, FTP_ASCII);
+                $successes[] = 'ftp.put.successful';
+            }
+        }catch (\Exception $exception){
+            $errors[] = 'i.could.not.put.file';
+        }
+
+        //test delete granted
+        try{
+            if(empty($errors)){
+                ftp_delete($FTPConnect, $putPath);
+                $successes[] = 'ftp.delete.file.successful';
+            }
+        }catch (\Exception $exception){
+            $errors[] = 'i.could.not.delete.file';
+        }
+
+        if(!empty($successes)){
+            $this->addFlash('success', implode('<br>', $successes));
+        }
+        if(!empty($errors)){
+            $this->addFlash('error', implode('<br>', $errors));
+        }
+
+        return $this->render('@BaboonPanel/Deploy/_ftp_configuration.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    public function deployToFTPAction()
+    {
+        return new Response('deployed to ftp');
     }
 }
--- src/Baboon/PanelBundle/Resources/public/js/configure/_configure_themes.js
+++ src/Baboon/PanelBundle/Resources/public/js/configure/_configure_themes.js
@@ -46,7 +46,34 @@ $(document).ready(function() {
             alert('deploy to git');
         },
         deployToFTP: function ($this) {
-            alert('deploy to ftp');
+            var themeDeployFTPIsConfigured = Routing.generate('bb_panel_deploy_ftp_is_configured');
+            $.get(themeDeployFTPIsConfigured, function(data){
+                if(data.isConfigured == false){
+                    ConfigureTheme.configureFTP();
+                }else{
+
+                }
+            });
+        },
+        configureFTP: function () {
+            var FTPConfigurationPath = Routing.generate('bb_panel_deploy_configure_ftp');
+            $.fancybox({
+                type: 'ajax',
+                href: FTPConfigurationPath
+            });
+        },
+        updateConfigureFTP: function () {
+            var FTPConfigurationForm = $('form[name="ftp_configuration"]');
+            $.post( FTPConfigurationForm.attr('action'), FTPConfigurationForm.serialize(), function( data ) {
+                $.fancybox(data);
+            });
+        },
+        testFTPConnection: function () {
+            var FTPConfigurationForm = $('form[name="ftp_configuration"]');
+            var FTPConnectionTestPath = Routing.generate('bb_panel_deploy_ftp_connection_test');
+            $.post( FTPConnectionTestPath, FTPConfigurationForm.serialize(), function( data ) {
+                $.fancybox(data);
+            });
         }
     };
 });
